46d239fe7ef6414552fe8703f37796367ca3380d
Administrator <Administrator@YLMF-2019YNKFCU>
2020-01-03 11:24:09 +0800

    测试

--- _test/src/com/demon/GCDLCM.java
+++ _test/src/com/demon/GCDLCM.java
@@ -5,10 +5,10 @@ package com.demon;
 import java.util.Scanner;
 
 public class GCDLCM {
-    // 鏈�澶у叕绾︽暟
+    // 最大公约数
     public static int get_gcd(int n1, int n2) {
         int gcd = 0;
-        if (n1 < n2) {// 浜ゆ崲n1銆乶2鐨勫��
+        if (n1 < n2) {// 交换n1、n2的值
             n1 = n1 + n2;
             n2 = n1 - n2;
             n1 = n1 - n2;
@@ -35,18 +35,17 @@ public class GCDLCM {
 
     }
 
-    // 鏈�灏忓叕鍊嶆暟
+    // 最小公倍数
     public static int get_lcm(int n1, int n2) {
         return n1 * n2 / get_gcd(n1, n2);
     }
 
     public static void main(String[] args) {
-    	System.out.print("测试本地修改 ");
-        System.out.print("娴嬭瘯");
+    	System.out.print("测试本地修改1");
         Scanner input = new Scanner(System.in);
-        System.out.print("璇疯緭鍏ョ涓�涓暣鏁帮細");
+        System.out.print("请输入第一个整数：");
         int n1 = input.nextInt();
-        System.out.print("璇疯緭鍏ョ浜屼釜鏁存暟锛�");
+        System.out.print("请输入第二个整数：");
         int n2 = input.nextInt();
         System.out.println("(" + n1 + "," + n2 + ")" + "=" + get_gcd(n1, n2));
         System.out.println("[" + n1 + "," + n2 + "]" + "=" + get_lcm(n1, n2));
